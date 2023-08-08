fd2e7f8e18c32be074d82fbeae7415208c7a15f1
MACaron99 <artemmakarenko12@gmail.com>
2023-08-08 20:31:58 +0300

    commit

---
+++ unit_1_compilation/level_1/package/ua/com/alevel/Crit.java
@@ -0,0 +1,16 @@
+package ua.com.alevel;
+public class Crit {
+    public static void main(String[] args) {
+        Object m = new Object();
+        byte b = Byte.MAX_VALUE;
+        byte b1 = Byte.MIN_VALUE;
+        short s = Short.MAX_VALUE;
+        int a = Integer.MAX_VALUE;
+        long l = Long.MAX_VALUE;
+        m.print(String.valueOf(b));
+        m.print(String.valueOf(b1));
+        m.print(String.valueOf(s));
+        m.print(String.valueOf(a));
+        m.print(String.valueOf(l));
+    }
+}
