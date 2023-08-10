3a7c4bc99877751bf5b4dff7bc1010b0ae14f164
MACaron99 <artemmakarenko12@gmail.com>
2023-08-10 12:39:59 +0300

    commit

---
+++ unit_1_compilation/level_2/depends/run.sh
@@ -0,0 +1,2 @@
+javac -sourcepath ./src -d build/classes -cp ./libs/commons-lang3-3.12.0-tests.jar ./src/ua/com/alevel/Value.java
+java -cp build/classes/:./libs/commons-lang3-3.10.0-tests.jar ua.com.alevel.Valuecd
