4ac4ac50f39c98e722222ba545fe5c891442f81b
MACaron99 <artemmakarenko12@gmail.com>
2023-08-13 12:12:02 +0300

    commint

--- unit_1_compilation/level_2/depends/run.sh
+++ unit_1_compilation/level_2/depends/run.sh
@@ -1,2 +1,2 @@
-javac -sourcepath ./src -d build/classes -cp ./libs/commons-lang3-3.12.0-tests.jar ./src/ua/com/alevel/Value.java
-java -cp build/classes/:./libs/commons-lang3-3.10.0-tests.jar ua.com.alevel.Valuecd
+javac -sourcepath ./src -d build/classes -cp ./libs/commons-math3-3.3.jar ./src/ua/com/alevel/Value.java
+java -cp build/classes/:./libs/commons-math3-3.3.jar ua.com.alevel.Value
