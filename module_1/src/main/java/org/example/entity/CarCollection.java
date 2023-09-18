03248d342c335ac3a0aff8da7b772287387c6362
artem <artemmakarenko12@gmail.com>
2023-09-18 22:47:42 +0300

    commit 18.09.23

---
+++ module_1/src/main/java/org/example/entity/CarCollection.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+public class CarCollection extends BaseEntity{
+
+    private String collectionId;
+    private String carId;
+
+    public String getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(String collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+}
