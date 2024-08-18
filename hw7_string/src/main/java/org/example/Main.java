e42ecbf7b59dd207c5e9a629e0baa3dca0bf3779
artem <artemmakarenko12@gmail.com>
2024-08-18 16:15:45 +0300

    Update hw7

--- hw7_string/src/main/java/org/example/Main.java
+++ hw7_string/src/main/java/org/example/Main.java
@@ -7,9 +7,11 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         program();
         menu();
+
         String position;
         while ((position = reader.readLine()) != null) {
             option(position, reader);
@@ -20,7 +22,8 @@ public class Main {
     private static void program() {
         System.out.println();
         System.out.println("Program STRING");
-        System.out.println("Welcome to the program STRING. A simple a program with a little functionality, but can reverse the string using three methods");
+        System.out.println("Welcome to the program STRING. A simple a program with a little functionality, " +
+                "but can reverse the string using three methods");
     }
 
     private static void menu() {
@@ -34,33 +37,52 @@ public class Main {
     }
 
     private static void option(String position, BufferedReader reader) throws IOException {
+
         switch (position) {
+
             case "1" -> {
                 System.out.println("Please enter the string");
                 Turner.src = reader.readLine();
+
                 System.out.println("Your string has been created successfully");
             }
+
             case "2" -> {
-                if (Turner.exist()) System.out.println(Turner.reverse(Turner.src));
-                else System.out.println("You didn't create a string");
+                if (Turner.exist()) {
+                    System.out.println(Turner.reverse(Turner.src));
+                } else {
+                    System.out.println("You didn't create a string");
+                }
             }
+
             case "3" -> {
                 if (Turner.exist()) {
                     System.out.println("Please enter the substring");
-                    System.out.println(Objects.requireNonNullElse(Turner.reverse(Turner.src, reader.readLine()), "No match found"));
-                } else System.out.println("You didn't create a string");
+                    System.out.println(Objects.requireNonNullElse(Turner.reverse(Turner.src, reader.readLine()),
+                            "No match found"));
+                } else {
+                    System.out.println("You didn't create a string");
+                }
             }
+
             case "4" -> {
                 if (Turner.exist()) {
                     System.out.println("Enter the first index");
                     int firstIndex = Integer.parseInt(reader.readLine());
+
                     System.out.println("Enter the last index");
                     int lastIndex = Integer.parseInt(reader.readLine());
+
                     if (firstIndex >= 0 && lastIndex < Turner.src.length() && firstIndex <= lastIndex) {
                         System.out.println(Turner.reverse(Turner.src, firstIndex, lastIndex));
-                    } else System.out.println("Invalid index");
-                } else System.out.println("You didn't create a string");
+                    } else {
+                        System.out.println("Invalid index");
+                    }
+                } else {
+                    System.out.println("You didn't create a string");
+                }
             }
+
             case "5" -> {
                 System.out.print("Goodbye!");
                 System.exit(0);
--- hw7_string/src/main/java/org/example/Turner.java
+++ hw7_string/src/main/java/org/example/Turner.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Turner {
+
     public static String src = null;
 
     public static boolean exist() {
@@ -8,8 +9,10 @@ public class Turner {
     }
 
     public static String reverse(String src) {
+
         char[] chars1 = src.toCharArray();
         char[] chars2 = new char[chars1.length];
+
         for (int i = 0; i < chars1.length; i++) {
             chars2[i] = chars1[chars1.length - 1 - i];
         }
@@ -17,10 +20,14 @@ public class Turner {
     }
 
     public static String reverse(String src, String dest) {
+
         char[] chars1 = src.toCharArray();
         char[] chars2 = dest.toCharArray();
+
         for (int i = 0; i <= chars1.length - chars2.length; i++) {
+
             boolean match = true;
+
             for (int j = 0; j < chars2.length; j++) {
                 if (chars1[i + j] != chars2[j]) {
                     match = false;
@@ -38,6 +45,7 @@ public class Turner {
     public static String reverse(String src, int firstIndex, int lastIndex) {
         char[] chars1 = src.toCharArray();
         char[] chars2 = new char[lastIndex - firstIndex + 1];
+
         System.arraycopy(chars1, firstIndex, chars2, 0, chars2.length);
         System.arraycopy(reverse(new String(chars2)).toCharArray(), 0, chars1, firstIndex, chars2.length);
         return new String(chars1);
